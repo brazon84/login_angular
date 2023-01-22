ba1382c52c372f5efcee012697c3b402cc441639
brazon84 <abg.brazon@gmail.com>
2023-01-22 18:37:19 -0300

    login2

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent  {
+  validatingForm: FormGroup;
+
+
+  constructor() {
+    this.validatingForm = new FormGroup({
+      loginFormModalEmail: new FormControl('', Validators.email),
+      loginFormModalPassword: new FormControl('', Validators.required)
+    });
+  }
+  get loginFormModalEmail() {
+    return this.validatingForm.get('loginFormModalEmail');
+  }
+
+  get loginFormModalPassword() {
+    return this.validatingForm.get('loginFormModalPassword');
+  }
+}
+
